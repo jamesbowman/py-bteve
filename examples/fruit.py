b304c72735c6e876fd4555497b2f48bf82ec0cc4
James Bowman <jamesb@excamera.com>
2020-10-12 17:46:58 -0700

    Add horizontal match case

--- examples/fruit.py
+++ examples/fruit.py
@@ -1,7 +1,7 @@
 import random
-import time
-
 import bteve as eve
+
+rnd = random.randrange
 
 gd = eve.Gameduino()
 gd.init()
@@ -23,33 +23,42 @@ class FruitGame:
             gd.load(f)
         gd.cmd_setbitmap(0, eve.ARGB4, 80, 80)
 
-        random.seed(0)
+        random.seed(2)
         self.board = [
-            [random.randrange(5) for y in range(8)]
+            [rnd(5) for y in range(8)]
             for x in range(8)
         ]
-        if 0:
-            self.board[5][2] = 1
-            self.board[5][3] = 1
-            self.board[5][4] = 1
-            self.board[5][5] = 1
 
         self.still = [0 for i in range(8)]
 
-    def collapse(self):
+    def match(self):
         b = self.board
         for x in range(8):
             r = run(b[x])
             if r is not None:
-                for i in range(60):
+                for i in range(30):
                     self.draw(bar = ((x, r.start), (x, r.stop - 1)))
                 self.still[x] = r.stop
-                b[x] = [random.randrange(5) for i in r] + b[x][:r.start] + b[x][r.stop:]
+                b[x] = [rnd(5) for i in r] + b[x][:r.start] + b[x][r.stop:]
                 for i in range(80 * len(r), -1, -10):
                     self.draw(fall = i)
                 self.still[x] = 0
                 return
-        
+        b = [[b[j][i] for j in range(8)] for i in range(8)]
+        for y in range(8):
+            r = run(b[y])
+            if r is not None:
+                for i in range(30):
+                    self.draw(bar = ((r.start, y), (r.stop - 1, y)))
+                for x in r:
+                    self.still[x] = y + 1
+                    self.board[x].pop(y)
+                    self.board[x].insert(0, rnd(5))
+                for i in range(80, -1, -10):
+                    self.draw(fall = i)
+                self.still = [0 for i in range(8)]
+                return
+
     def draw(self, bar = None, fall = 0):
         gd.cmd_gradient(0, 0, 0x202040, 0, 720, 0xffd0c0)
         if bar is not None:
@@ -57,7 +66,7 @@ class FruitGame:
             gd.LineWidth(8 * 80)
             for (x, y) in bar:
                 gd.Vertex2f(360 + 80 * x, 80 + 80 * y)
-            
+
         gd.Begin(eve.BITMAPS)
         for x in range(8):
             for y in range(0, 8):
@@ -72,10 +81,10 @@ class FruitGame:
     def play(self):
         self.draw()
         while 1:
-            self.collapse()
+            self.match()
             self.draw()
-            if random.randrange(2) == 0:
-                self.board[random.randrange(8)][random.randrange(8)] = random.randrange(5)
+            if rnd(2) == 0:
+                self.board[rnd(8)][rnd(8)] = rnd(5)
 
 
 FruitGame().play()
