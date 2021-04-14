9fcd75e222fb02b917d7d10e5dddd19207b8bdca
James Bowman <jamesb@excamera.com>
2021-04-14 10:30:53 -0700

    Consistent reset for Feathe M4 Express

--- bteve/gameduino_circuitpython.py
+++ bteve/gameduino_circuitpython.py
@@ -4,6 +4,7 @@ import busio
 import digitalio
 import sdcardio
 import storage
+import time
 
 from .gameduino import Gameduino
 
@@ -17,28 +18,40 @@ def spilock(f):
         return r
     return wrapper
 
+def pin(p):
+    r = digitalio.DigitalInOut(p)
+    r.direction = digitalio.Direction.OUTPUT
+    r.value = True
+    return r
+
+def reset(p):
+    print('pgm reset')
+    pgm = pin(p)
+    pgm.value = False
+    time.sleep(.1)
+    pgm.value = True
+    time.sleep(.6)
+    print('pgm reset done')
+
 class GameduinoCircuitPython(Gameduino):
     def __init__(self):
         mach = os.uname().machine
         if mach == 'Raspberry Pi Pico with rp2040':
             self.sp = busio.SPI(board.GP2, MOSI=board.GP3, MISO=board.GP4)
             cs = (board.GP5, board.GP6, board.GP7)
+            reset(board.GP10)
         elif mach.startswith("Adafruit Feather M4 Express"):
             self.sp = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-            cs = (board.D9, board.D6, board.D5)
+            cs = (board.D4, board.D5, board.D6)
         else:
             self.sp = busio.SPI(board.D13, MOSI=board.D11, MISO=board.D12)
             cs = (board.D8, board.D9, board.D10)
-        def pin(p):
-            r = digitalio.DigitalInOut(p)
-            r.direction = digitalio.Direction.OUTPUT
-            r.value = True
-            return r
         self.cs = pin(cs[0])
         self.daz = pin(cs[2])
         if not self.setup_sd(cs[1]):
             pin(cs[1])
         self.setup_spi()
+        print('cp init complete')
 
     def setup_sd(self, sdcs):
         try:
