66ccc1f109ed3be562b1d9eff7c43f6111c869a1
James Bowman <jamesb@excamera.com>
2021-07-13 10:50:02 -0700

    Remove phase terms

--- examples/qix.py
+++ examples/qix.py
@@ -15,11 +15,12 @@ def tri(t):
     return 2 * abs(math.fmod(t, 1.0) - 0.5)
 
 def at(t, ff):
-    x = gd.w * tri(ff[0] + ff[1] * t)
-    y = gd.h * tri(ff[2] + ff[3] * t)
+    x = gd.w * tri(ff[0] * t)
+    y = gd.h * tri(ff[1] * t) - 10
     gd.Vertex2f(x, y)
 
-F = [random.random() for i in range(18)]
+random.seed(0)
+F = [random.random() for i in range(9)]
 
 while True:
     t = time.monotonic_ns() / 18000000
@@ -30,12 +31,12 @@ while True:
         gd.Begin(eve.LINE_STRIP)
         tn = (t - dt) / 150
         gd.ColorRGB(
-            int(255 * tri(F[12] + F[13] * tn)),
-            int(255 * tri(F[14] + F[15] * tn)),
-            int(255 * tri(F[16] + F[17] * tn)))
-        at(tn, F[0:4])
-        at(tn, F[4:8])
-        at(tn, F[8:12])
-        at(tn, F[0:4])
+            int(255 * tri(F[6] * tn)),
+            int(255 * tri(F[7] * tn)),
+            int(255 * tri(F[8] * tn)))
+        at(tn, F[0:2])
+        at(tn, F[2:4])
+        at(tn, F[4:6])
+        at(tn, F[0:2])
 
     gd.swap()
